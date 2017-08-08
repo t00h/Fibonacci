f3f2edba8185c9ced2918649e79ec27ecb6072ed
taeh1 <taeh@st-andrews.ac.uk>
2017-08-08 05:10:43 +0100

    BigIntegers, option to print sequence or specified term, and commas
    I converted the use of longs in the calculations to BigIntegers, so the
    program can calculate the Fibonacci sequence at much greater term
    numbers & values. I created and incorporated a function to add commas
    onto term numbers, e.g. 1000th term --> 1,000th term. The user can now
    select to only print the value at a specified term in the sequence, or
    all the values up to and including the specified term. I'm planning on
    also allowing the user to choose the format of the output FIbonacci
    values - as plain numbers, with commas, or in scientific notation, as
    well as maybe also adding a timer so the user can see how much time a
    program run takes. I might also change the method of getting the term
    number from taking in a long to taking in a BigInteger, to allow the
    program to work to much greater term numbers than it currently can.

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,32 +1,73 @@
 import java.util.Scanner;
+import java.math.BigInteger;
+import java.util.ArrayList;
 
 public class Fibonacci {
 
-	public static void main(String[] args) {
-    System.out.println("This program prints the Fibonacci sequence to a specified term number, e.g. the 1st, 3rd, or 100th term.");
+    public static void main(String[] args) {
+        System.out.println("This program will print out the Fibonacci sequence to a specified term number, or will print the number at a specified term number in the sequence.");
+        String input = getString("Please choose whether to print the whole sequence (input \"S\") or only the number (input \"N\") at the specified term number.");
+        while (!input.equals("S") && !input.equals("N")) {
+            input = getString("That was an invalid input, please try again.\n Please choose whether to print the whole sequence (input \"S\") or only the number (input \"N\") at the specified term number.");
+        }
+        if (input.equals("S")) {
+            printSequence();
+        }
+        else {
+            printNumber();
+        }
+    }
+
+	public static void printSequence() {
+    System.out.println("This method prints the Fibonacci sequence to a specified term number, e.g. the 1st, 3rd, or 100th term.");
     System.out.println();
         
-    long even = 0;
-    long odd = 1;        
-    long toPrint = 0; // use this in printing to prevent stack overflows
-	long to_go_to = getLong("Please give the term number in the Fibonacci sequence to print to (e.g. 0th, 2nd, or 53rd. Due to stack overflow errors, the maximum number of terms is 93 (to 92nd term).");
+    BigInteger even = new BigInteger("0");
+    BigInteger odd = new BigInteger("1");
+    BigInteger toPrint = new BigInteger("0"); // use this in printing to prevent stack overflows
+	long to_go_to = getLong("Please give the term number in the Fibonacci sequence to print to (e.g. 0th, 2nd, or 53rd).");
         
     System.out.println();
-    System.out.println("Program now printing the Fibonacci sequence to the " + to_go_to + getOrdinal(to_go_to) + " term.");
+    System.out.println("Program now printing the Fibonacci sequence to the " + addCommas(to_go_to) + getOrdinal(to_go_to) + " term.");
         
-    for (long i = 0; i <= to_go_to && (toPrint > 0 || i < 2); i++) {
+    for (long i = 0; i <= to_go_to; i++) {
         if (i %2 == 0) { // an even term no
             toPrint = even;
-            even += odd;
+            even = even.add(odd); // even += odd
         }
         else { // at an odd term no
             toPrint = odd;
-            odd += even;
+            odd  = odd.add(even); // odd += even
         }
-        if (toPrint > 0 || i < 2) System.out.println(i + getOrdinal(i) + " term; value: " + toPrint);
+        System.out.println(addCommas(i) + getOrdinal(i) + " term in the Fibonacci sequence: " + toPrint.toString());
     }
-	System.out.println("Program done.");
+    System.out.println("Program done.");
 	}
+
+	public static void printNumber() {
+	    System.out.println("This method prints the number in the Fibonacci sequence at a specified term number, e.g. the 1st, 3rd, or 100th term.");
+        System.out.println();
+
+        BigInteger even = new BigInteger("0");
+        BigInteger odd = new BigInteger("1");
+        BigInteger toPrint = new BigInteger("0"); // use this in printing to prevent stack overflows
+        long to_go_to = getLong("Please give the term number in the Fibonacci sequence to print (e.g. 0th, 2nd, or 53rd).");
+
+        System.out.println();
+        System.out.println("Program now printing the number at the " + addCommas(to_go_to) + getOrdinal(to_go_to) + " term in the Fibonacci sequence.");
+
+        for (long i = 0; i <= to_go_to; i++) {
+            if (i %2 == 0) { // an even term no
+                toPrint = even;
+                even = even.add(odd); // even += odd
+            }
+            else { // at an odd term no
+                toPrint = odd;
+                odd  = odd.add(even); // odd += even
+            }
+        }
+        System.out.println("The number at the " + addCommas(to_go_to) + getOrdinal(to_go_to) + " term in the Fibonacci sequence is: \n" + toPrint.toString() + "\nProgram done.");
+    }
     
     public static String getOrdinal(long i) {
         byte penDigit = (byte) ((i / 10) % 10); // penultimate - second to last
@@ -66,5 +107,39 @@ public class Fibonacci {
             toReturn = getLong(instructions);
         }
         return toReturn;
+    }
+
+    public static String getString(String instructions) {
+        System.out.println(instructions);
+        Scanner reader = new Scanner(System.in);
+        try {
+            return (String) (reader.next().toString());
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Your input was invalid; please give a valid String input.");
+            return getString(instructions);
+        }
+    }
+
+    public static String addCommas(long numIn) {
+        String numAsString = (String) (String.valueOf(numIn));
+        String commaString = "";
+
+        if (numAsString.length() > 3) {
+            for (int i = numAsString.length() - 1; i >= 0; i--) {
+                if (i < numAsString.length() - 2 && numAsString.length() > 3) {
+                    if ((i + 2) % 3 == 0) {
+                        commaString = "," + numAsString.substring(numAsString.length() - 3, numAsString.length()) + commaString;
+                        numAsString = numAsString.substring(0, numAsString.length() - 3);
+                    }
+                } else if (numAsString.length() < 4) {
+                    commaString = numAsString + commaString;
+                    numAsString = "";
+                }
+            }
+        }
+        else commaString = numAsString;
+        return commaString;
     }
 }
